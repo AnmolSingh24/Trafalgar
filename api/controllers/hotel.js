886121b8230d7dbe1c5352c70c6a60f8293f4036
Anmol <as0796348@gmail.com>
2023-09-25 11:46:14 +0530

    new hotels and cities added

--- api/controllers/hotel.js
+++ api/controllers/hotel.js
@@ -1,8 +1,9 @@
 import Hotel from "../models/Hotel.js";
 
 export const createHotel = async (req, res, next) => {
+    const newHotel = new Hotel(req.body)
     try {
-        const savedHotel = await new Hotel.save();
+        const savedHotel = await newHotel.save();
         res.status(200).json(savedHotel)
 
     } catch (err) {
@@ -43,6 +44,19 @@ export const getHotels = async (req, res, next) => {
     try {
         const Hotels = await Hotel.find();
         res.status(200).json(Hotels)
+
+    } catch (err) {
+        next(err);
+    }
+}
+
+export const countByCity = async (req, res, next) => {
+    const cities = req.query.cities.split(",")
+    try {
+        const list = await Promise.all(cities.map(city => {
+            return Hotel.countDocuments({ city: city })
+        }))
+        res.status(200).json(list);
 
     } catch (err) {
         next(err);
--- api/routes/hotels.js
+++ api/routes/hotels.js
@@ -1,7 +1,7 @@
 import express from "express";
 import Hotel from "../models/Hotel.js"
 import { verifyAdmin } from "../utils/verifyToken.js";
-import { createHotel, deleteHotel, getHotel, getHotels, updatedHotel } from "../controllers/hotel.js";
+import { countByCity, createHotel, deleteHotel, getHotel, getHotels, updatedHotel } from "../controllers/hotel.js";
 const router = express.Router();
 
 //Create
@@ -11,12 +11,14 @@ router.post("/", verifyAdmin, createHotel);
 router.put("/:id", verifyAdmin, updatedHotel);
 
 //Delete
-router.delete("/:id", verifyAdmin, deleteHotel)
+router.delete("/find/:id", verifyAdmin, deleteHotel)
 
 //Get
 router.get("/:id", getHotel);
 
 //Get all
 router.get("/", getHotels);
+router.get("/countByCity", countByCity);
+router.get("/countByType", getHotels);
 
 export default router;
